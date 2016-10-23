f9bceda4aa22e00fc566b5438820228f90f39c02
deanrobertcook <deanrobertcook@gmail.com>
2016-10-23 17:30:44 +0200

    Add before all method to TestSpec to ensure UI thread is set once

--- core/src/test/scala/com/wire/testutils/TestSpec.scala
+++ core/src/test/scala/com/wire/testutils/TestSpec.scala
@@ -3,11 +3,19 @@ package com.wire.testutils
 import com.wire.threading.{Threading, UiDispatchQueue}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest
-import org.scalatest.{BeforeAndAfter, FeatureSpec, OneInstancePerTest, OptionValues}
+import org.scalatest._
 
-abstract class TestSpec extends FeatureSpec with scalatest.Matchers with OptionValues with BeforeAndAfter with OneInstancePerTest with MockFactory {
+/**
+  * Note, the inclusion of OneInstancePerTest will mean that all instances within the test spec's scope will be re-created before each test. For that reason, anything that
+  * needs to be done just once should go in the beforeAll method.
+  */
+abstract class TestSpec extends FeatureSpec with scalatest.Matchers with OptionValues with BeforeAndAfter with BeforeAndAfterAll with OneInstancePerTest with MockFactory {
 
-  Threading.setUiDispatchQueue(new UiDispatchQueue {
-    override def execute(runnable: Runnable): Unit = runnable.run()
-  })
+  override def beforeAll {
+    Threading.setUiDispatchQueue(new UiDispatchQueue {
+      override def execute(runnable: Runnable): Unit = runnable.run()
+    })
+
+  }
+
 }
